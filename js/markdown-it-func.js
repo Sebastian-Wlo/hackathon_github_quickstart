9c9f733103db9feb723a909bfd3104194ee5ba4b
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-06-21 18:56:40 +0200

    Changed all anchor tags to open links in new windows

--- js/markdown-it-func.js
+++ js/markdown-it-func.js
@@ -1,5 +1,5 @@
 /*
-  I didn't write his Script!
+  I didn't write most of this Script!
   Original author: Moacir P. de Sá Pereira,
   Source: https://the-javascripting-english-major.org/v1/12-markdown
 */
@@ -14,7 +14,8 @@ $(document).ready(function() {
         success: function(markdown){
           // Convert the Markdown to HTML.
           let html;
-          html = md.render(markdown);
+          html = md.render(markdown).replaceAll(`<a `, `<a target="_blank" `);
+
           // Print the HTML to #content using jQuery.
           $("#markdown-container").html(html);
         }
